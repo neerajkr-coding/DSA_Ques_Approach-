9a01b9ba28a1bc5a268c69a95afc400d4d733c1c
Neeraj Kumar <134834249+neerajkr-coding@users.noreply.github.com>
2024-08-11 19:11:27 +0530

    Update Move all Zeros to the end of the array.java

--- Arrays/Easy/Move all Zeros to the end of the array.java
+++ Arrays/Easy/Move all Zeros to the end of the array.java
@@ -1,3 +1,24 @@
 //******************************************************BRUTE FORCE APPROACH******************************************************
-//******************************************************BETTER APPROACH***********************************************************
+void pushZerosToEnd(int[] arr, int n) {   //Brute force approach
+    
+    ArrayList<Integer> nonZero = new ArrayList<>();
+    
+    for(int elem: arr){
+        
+        if(elem != 0){
+            nonZero.add(elem);
+        }
+    }
+    
+    
+    for(int i = 0; i < nonZero.size(); i++){
+        arr[i] = nonZero.get(i);
+    }
+    
+    for(int i = nonZero.size(); i < n; i++){
+        arr[i] = 0;  
+    }
+       
+    }
+
 //******************************************************OPTIMAL APPROACH**********************************************************
